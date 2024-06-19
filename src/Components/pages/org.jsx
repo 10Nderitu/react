ae2567c568e232a2df03990ca0d1ebb8162abbd7
Ray <raynderitu10@gmail.com>
2024-06-19 16:16:51 +0300

    commit changes

--- src/Components/pages/org.jsx
+++ src/Components/pages/org.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import logo from '../images/logo.jpeg'
 
 function Org() {
     return (
         <div className="flex flex-col items-center justify-center h-screen">
+            <img src={logo} alt="Organization Logo" />
             <p className="text-xl font-semibold">Organization Name</p>
+
         </div>
     );
 }
