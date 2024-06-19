84735a0bc504cd3e4458278d639d6bfb113fa273
Ray <raynderitu10@gmail.com>
2024-06-19 12:30:45 +0300

    initial commit commit changes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import SignUp from './Components/signup/signup'
+
+function App () {
+  return (
+    <div className='w-full flex items-center justify-center'>
+        <SignUp />
+    </div>
+  );
+}
+
+export default App;
+
